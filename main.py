d1bb27fe48731963d85e49d04c6b04dbed3a750a
Gabriel Erazo <gfranciscoerazom@protonmail.com>
2025-01-10 12:24:51 -0500

    OAuth

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from os import access
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
+import httpx
 import uvicorn  # Library for serving the API
 from fastapi import FastAPI  # Import the FastAPI class
 # Import the engine for the database
@@ -31,6 +34,37 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(users_endpoints.router)
 app.include_router(admin_endpoints.router)
 app.include_router(compare_careers_endpoints.router)
+
+
+@app.get("/login-oauth")
+def login_oauth():
+    return RedirectResponse('https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id=919034670843-dv2u5jdp82u75bllboooi9aekreuv4ej.apps.googleusercontent.com&response_type=code&redirect_uri=http://localhost:8000/login-code&scope=email%20profile%20openid&openid.realm', status_code=302)
+
+
+@app.get("/login-code")
+async def login_code(code: str):
+    print(">>>>>>>>>>>>>>>>>.....", code)
+    params = {
+        'client_id': '919034670843-dv2u5jdp82u75bllboooi9aekreuv4ej.apps.googleusercontent.com',
+        'client_secret': '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',
+        'code': code,
+        'grant_type': 'authorization_code',
+        'redirect_uri': 'http://localhost:8000/login-code'
+    }
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.post("https://oauth2.googleapis.com/token", params=params, headers=headers)
+
+    response_json = response.json()
+    print(">>>>>>>>>>>>>>>>>>>>>>,,,,,,", response_json)
+    access_token = response_json['access_token']
+    async with httpx.AsyncClient() as client:
+        headers.update({'Authorization': f'Bearer {access_token}'})
+        response = await client.get("https://www.googleapis.com/oauth2/v1/userinfo", headers=headers)
+    return response.json()
 
 
 db = SessionLocal()
